f077e4fe1d0ec66e8b1d2debaefd7aa4e5c132ab
Leonardo Dias <leonardo_marquesdias@hotmail.com>
2020-07-22 21:35:09 +0100

    Exercicio Finalizado
    Pequeno Projeto vanilha

--- main.js
+++ main.js
@@ -22,8 +22,9 @@ const produtos = [
     }
 ];
 
-function renderizaProduto(produto) {
-  console.log(produto)
+const carrinhoItens = {};
+
+function renderizaProduto(produto, index) {
   return `
     <div class="col-sm-4 mb-3">
       <div class="card">
@@ -33,7 +34,7 @@ function renderizaProduto(produto) {
             <h5 class="card-title">${produto.nome}</h5>
             <small>${produto.preco}</small>
             <p class="card-text">${produto.descricao}</p>
-              <button data-value="300" class="btn btn-primary">Adicionar</button>  
+            <button data-index="${index}" class="btn btn-primary btn-add">Adicionar</button>  
           </div>
         </div>
       </div>
@@ -41,13 +42,81 @@ function renderizaProduto(produto) {
   `
 }
 
-document.querySelector('.loja').innerHTML = renderizaProdutos();
-
 function renderizaProdutos() {
   let html = '';
   for (let i = 0; i < produtos.length; i++) {
-   html = html + renderizaProduto(produtos[i]);
+   html = html + renderizaProduto(produtos[i], i);
   }
   return html;
 }
+
+function renderizaItemCarrinho(produtoCarrinho) {
+  return `
+  <div class="card carrinho__item">
+    <div class="card-body">
+        <h5 class="card-title">${produtoCarrinho.nome}</h5>
+        <p class="card-text">Preço unidade: R$ ${produtoCarrinho.preco} | Quantidade: ${produtoCarrinho.quantidade}</p>
+        <p class="card-text">Valor: ${produtoCarrinho.preco * produtoCarrinho.quantidade}</p>
+          <button data-produto-id="${produtoCarrinho.id}" class="btn btn-danger btn-remove btn-sm">Remover</button>  
+    </div>
+  </div>
+  `
+}
+
+function renderizaCarrinho() {
+  let html = '';
+  for (let produtoId in carrinhoItens) {
+    html = html + renderizaItemCarrinho(carrinhoItens[produtoId]);
+  }
+    document.querySelector('.carrinho__itens').innerHTML = html;
+}
+
+function renderCarrinhoTotal() {
+  let total = 0;
+
+  for (let produtoId in carrinhoItens) {
+    total = total + (carrinhoItens[produtoId].preco * carrinhoItens[produtoId].quantidade);
+  }
+    document.querySelector('.carrinho__total').innerHTML = `<h6>Total <strong>R$ ${total} </strong></h6>`
+}
+
+function adicionaItemNoCarrinho(produto) {
+  if (!carrinhoItens[produto.id]) {
+    carrinhoItens[produto.id] = produto;
+    carrinhoItens[produto.id].quantidade = 0;
+  }
+
+  ++carrinhoItens[produto.id].quantidade;
+
+  renderizaCarrinho();
+  renderCarrinhoTotal();
+}
+
+document.body
+.addEventListener('click', function(event) {
+    const elemento = event.target;
+
+    if (elemento.classList.contains('btn-add')) {  
+    const index = parseInt(elemento.getAttribute('data-index'), 10);
+    const produto = produtos[index];
+    
+    adicionaItemNoCarrinho(produto);
+  }
+
+    if (elemento.classList.contains('btn-remove')) {
+    const produtoId = elemento.getAttribute('data-produto-id');
+    if (carrinhoItens[produtoId].quantidade <= 1){
+      delete carrinhoItens[produtoId];
+    } else {
+      --carrinhoItens[produtoId].quantidade;
+    }
+
+    renderizaCarrinho();
+    renderCarrinhoTotal();
+    }
+});
+
+document.querySelector('.loja').innerHTML = renderizaProdutos();
+
+
 
